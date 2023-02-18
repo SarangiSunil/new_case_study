597d0b7c7e37ab9873b11fa95987d0cf54a83dd6
Sarangi Sunil <124140871+SarangiSunil@users.noreply.github.com>
2023-02-18 08:28:07 +0530

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,27 @@
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+
+class Parallelepiped(Rectangle):
+    def __init__(self, length, breadth, height):
+        super().__init__(length, breadth)
+        self.height = height
+
+    def volume(self):
+        return self.length * self.breadth * self.height
+import Rectangle 
+
+rect = rectangle.Rectangle(5, 10)
+print("Rectangle area:", rect.area())
+print("Rectangle perimeter:", rect.perimeter())
+
+para = rectangle.Parallelepiped(5, 10, 15)
+print("Parallelepiped volume:", para.volume())
+os.getcwd()
