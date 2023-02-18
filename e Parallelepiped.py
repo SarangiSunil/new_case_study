8b0d3b91a3d3bc9d076a39e0c3ee65153108a4e6
Sarangi Sunil <124140871+SarangiSunil@users.noreply.github.com>
2023-02-18 08:11:47 +0530

    Create e Parallelepiped.py

---
+++ e Parallelepiped.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def area(self):
+        return self.length * self.width
+
+    def display(self):
+        print(f"Length: {self.length}")
+        print(f"Width: {self.width}")
+        print(f"Perimeter: {self.perimeter()}")
+        print(f"Area: {self.area()}")
+
+
+class Parallelepiped(Rectangle):
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = height
+
+    def volume(self):
+        return self.length * self.width * self.height
+        
+rectangle = Rectangle(5, 10)
+rectangle.display()
+
+parallelepiped = Parallelepiped(5, 10, 3)
+parallelepiped.display()
+print(f"Volume: {parallelepiped.volume()}")
